8a3b56e85ff9a662acdb8ad4191453ea66149365
Abdullah Bakhach <abakhach@sweettoothhq.com>
2012-09-06 10:54:54 +0300

    first commit

---
+++ sampleQueueIphone/Player.m
@@ -0,0 +1,22 @@
+//
+//  Player.m
+//  AudioConverterCoacoa
+//
+//  Created by Abdullah Bakhach on 9/2/12.
+//  Copyright (c) 2012 Amazon. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+@synthesize  playbackFile; 
+@synthesize  packetPosition; 
+@synthesize  numPacketsToRead; 
+@synthesize  packetDescs;
+@synthesize  isDone;
+
+
+
+
+@end
